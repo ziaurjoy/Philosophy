7d418c06ee7d8e7cc23d15292903659459844391
ZiaurJoy <ziaurjoy802@gmail.com>
2021-03-05 04:33:26 +0600

    Home page clear

--- Philosophy/views.py
+++ Philosophy/views.py
@@ -1,9 +1,15 @@
 
 from django.shortcuts import render
 
-
+from blog.models import BlogPost, Category
 
 # Home page request
 
 def home(request):
-    return render(request, 'fontend/home.html')
+    categoris = Category.objects.all()
+    posts = BlogPost.objects.all()
+    context = {
+        'categoris': categoris,
+        'posts': posts
+    }
+    return render(request, 'fontend/home.html', context)
--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,22 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+
+
+class Category(models.Model):
+    category_name = models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.category_name
+
+
+class BlogPost(models.Model):
+    title = models.CharField(max_length=100)
+    post_time = models.DateTimeField(auto_now_add=True)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    images = models.ImageField()
+    text = models.TextField()
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
